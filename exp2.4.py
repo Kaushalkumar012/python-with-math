4238c7b9edfd162b16e59452745342da48aac231
Kaushal kumar <146443100+Kaushalkumar012@users.noreply.github.com>
2025-04-03 09:29:16 +0530

    Add files via upload

---
+++ exp2.4.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+reversed_name = name[::-1]
+print("Your name in reverse is:", reversed_name)
+last_five_characters = reversed_name[:5]
+print("The last 5 characters of the reversed name are:", last_five_characters)
